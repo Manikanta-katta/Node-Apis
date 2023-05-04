2e0de697d529869dd9083c388f17580a9f477e69
Manikanta Katta <manikanta.katta@peopletech.com>
2023-05-04 18:08:46 +0530

    initial commit

---
+++ contollers/employeeController.js
@@ -0,0 +1,106 @@
+const Employee = require("../models/employee");
+
+//Show the list of Employees
+
+const index = (req, res, next) => {
+    Employee.find()
+        .then((response) => {
+            res.json({
+                response,
+            });
+        })
+        .catch((err) => {
+            res.json({
+                message: "An error Occured!",
+            });
+        });
+};
+
+//show single employee details
+const show = (req, res, next) => {
+    let employeeID = req.body.employeeID;
+    Employee.findById(employeeID)
+        .then((response) => {
+            res.json({
+                response,
+            });
+        })
+        .catch((err) => {
+            res.json({
+                message: "An error occured",
+            });
+        });
+};
+// store employee details
+const store = (req, res, next) => {
+    let employee = new Employee({
+        name: req.body.name,
+        designation: req.body.designation,
+        email: req.body.email,
+        phone: req.body.phone,
+        age: req.body.age,
+    })
+    if (req.file) {
+        employee.avatar = req.file.path
+    }
+
+    employee
+        .save()
+        .then((response) => {
+        res.json({
+                message: "Employee added successfully!",
+            });
+        })
+        .catch((err) => {
+            res.json({
+                message: " An error Occured!",
+            });
+        });
+};
+// update employee details
+const update = (req, res, next) => {
+    let employeeID = req.body.employeeID;
+    let updatedData = {
+        name: req.body.name,
+        designation: req.body.designation,
+        email: req.body.email,
+        phone: req.body.phone,
+        age: req.body.age,
+    };
+
+    Employee.findByIdAndUpdate(employeeID, { $set: updatedData })
+        .then((response) => {
+            res.json({
+                message: "Employee updated successfully!",
+            });
+        })
+        .catch((err) => {
+            res.json({
+                message: " An error Occured!",
+            });
+        });
+};
+
+// delete an employee
+
+const destroy = (req, res, next) => {
+    let employeeID = req.body.employeeID;
+    Employee.findByIdAndDelete(employeeID)
+        .then(() => {
+            res.json({
+                message: "Employee deleted Succesfully!",
+            });
+        })
+        .catch((err) => {
+            req.json({
+                message: "An error occured!",
+            });
+        });
+};
+module.exports = {
+    index,
+    show,
+    store,
+    update,
+    destroy,
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express();
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const EmployeeRoute = require('./routes/employee')
+mongoose.connect("mongodb+srv://manikanta:manikanta@cluster0.taclgus.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("DB Connected .."))
+    .catch((err) => console.log(err));
+
+const db = mongoose.connection
+
+// db.on('error',(err) =>{
+//     console.log(err)
+// })
+
+// db.once('open',()=>{
+//     console.log("Data base connected")
+// })
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+const PORT = process.env.PORT || 2000
+
+app.listen(PORT, () => console.log(`Server running...${PORT}`));
+app.use('/api/employee', EmployeeRoute)
