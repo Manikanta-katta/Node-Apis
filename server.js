60ff7fd9c2e6e3a8dffbfd30093deb7c8444718b
Manikanta Katta <manikanta.katta@peopletech.com>
2023-05-05 17:10:41 +0530

    Initial commit

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const EmployeeRoute = require('./routes/employee')
-mongoose.connect("mongodb+srv://manikanta:manikanta@cluster0.taclgus.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("DB Connected .."))
-    .catch((err) => console.log(err));
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const EmployeeRoute = require("./routes/employee");
+mongoose
+  .connect(
+    "mongodb+srv://manikanta:manikanta@cluster0.taclgus.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("DB Connected .."))
+  .catch((err) => console.log(err));
 
-const db = mongoose.connection
+const db = mongoose.connection;
 
 // db.on('error',(err) =>{
 //     console.log(err)
@@ -18,10 +22,12 @@ const db = mongoose.connection
 //     console.log("Data base connected")
 // })
 
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-const PORT = process.env.PORT || 2000
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 2000;
 
 app.listen(PORT, () => console.log(`Server running...${PORT}`));
-app.use('/api/employee', EmployeeRoute)
+app.use("/api/employee", EmployeeRoute);
+
+module.exports = app
