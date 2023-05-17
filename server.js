c2a7567e6fe9ab5e348a287697f82c3695aad75f
Manikanta Katta <manikanta.katta@peopletech.com>
2023-05-17 18:27:56 +0530

    secretkey changed

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const EmployeeRoute = require("./routes/employee");
 const AuthenticateRoute = require("./routes/auth");
-const Audio = require("./routes/openai")
-const Personal = require('./routes/personal')
+const Audio = require("./routes/openai");
+const Personal = require("./routes/personal");
 
 mongoose
   .connect(
@@ -31,20 +31,17 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const corsOptions ={
-  origin:'http://localhost:3000', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
-app.use(cors(corsOptions))
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
-
-
 
 app.listen(PORT, () => console.log(`Server running...${PORT}`));
 app.use("/api/employee", EmployeeRoute);
 app.use("/api/employee", AuthenticateRoute);
-app.use("/api",Audio),
-app.use('/api',Personal)
+app.use("/api", Audio), app.use("/api", Personal);
 
 module.exports = app;
